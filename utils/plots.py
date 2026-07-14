630810afab5d4047581d9ec3077a2a41617990a0
Sam Taylor-Wilmshurst <samtaylorwilmshurst at gmail>
2026-07-14 22:13:47 +0100

    amending plots.py to fix a text display error

--- utils/plots.py
+++ utils/plots.py
@@ -148,7 +148,8 @@ class Annotator:
 
     def text(self, xy, text, txt_color=(255, 255, 255)):
         # Add text to image (PIL-only)
-        w, h = self.font.getsize(text)  # text width, height
+        left, top, right, bottom = self.font.getbbox(text)
+        w, h = right - left, bottom - top  # text width, height
         self.draw.text((xy[0], xy[1] - h + 1), text, fill=txt_color, font=self.font)
 
     def result(self):
