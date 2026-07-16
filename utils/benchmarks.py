5c10cb9bde84a45286e586598ac10b33f14d87c3
Sam Taylor-Wilmshurst <samtaylorwilmshurst at gmail>
2026-07-16 17:27:41 +0100

    moving files to new /edge/ and /cloud/ directories

--- utils/benchmarks.py
+++ utils/benchmarks.py
@@ -38,7 +38,7 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 # ROOT = ROOT.relative_to(Path.cwd())  # relative
 
-import export
+import edge.export as export
 import val
 from utils import notebook_init
 from utils.general import LOGGER, print_args
