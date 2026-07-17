2856ad633b8b7cd9b2977a68dbd73e7cee7f092c
Sam Taylor-Wilmshurst <samtaylorwilmshurst at gmail>
2026-07-17 18:22:53 +0100

    finalising class count script

--- data_prep_scripts/class_distribution_pascal_voc.py
+++ data_prep_scripts/class_distribution_pascal_voc.py
@@ -1,19 +1,25 @@
 """
-code for visualising the class count across the target dataset
+code for visualising the class count across the target dataset - assuming labels in Pascal VOC format
 
-target directories are typically: ./annotations/<video_name>/<video_frame>.xml
+script also saves class count data in a dynamically named directory at same root as script
+
+target directories are typically: ./<dataset_name>/annotations/<video_name>/<video_frame>.xml
 """
 import os
 from collections import Counter
 import concurrent.futures
 import pandas as pd
 import matplotlib.pyplot as plt
+import json
 import xml.etree.ElementTree as ET
 # annotations filepath
-from config import ANNOTATIONS_DIR, RECOMMENDED_CORES
+from config import RAW_DATA_DIR, ANNOTATIONS_DIR, RECOMMENDED_CORES
 
 def process_single_folder(folder_path: str) -> tuple:
     # worker function to process .xml files in a single folder
+
+    # print update to console
+    print(f"Now processing item {folder_path}.")
     
     # initialise counters
     frame_object_counts = Counter({"None": 0,
@@ -82,10 +88,27 @@ if __name__ == "__main__":
     # Convert to DataFrames, orienting keys as the index
     frame_object_count_df = pd.DataFrame.from_dict(object_count_dict, orient='index', columns=['Count'])
     object_class_count_df = pd.DataFrame.from_dict(object_class_count_dict, orient='index', columns=['Count'])
-    
+
     # Visualise categorical data using bar charts
-    frame_object_count_df.plot.bar(title="Number of objects in each Frame")
-    object_class_count_df.plot.bar(title="Total Class Counts")
+    frame_object_count_plot = frame_object_count_df.plot.bar(title="Number of objects in each Frame")
+    class_count_plot = object_class_count_df.plot.bar(title="Total Class Counts")
+
+    ### save data to directory
+
+    # directory name
+    dataset_name = os.path.basename(os.path.normpath(RAW_DATA_DIR))
+    save_dir = f"{dataset_name}_class_counts"
+    # initialise directory
+    os.makedirs(save_dir, exist_ok = True)
     
+    # write class count dictionaries to .json
+    with open(os.path.join(save_dir, "total_frame_object_count.json"), "w") as fp:
+        json.dump(object_count_dict, fp, sort_keys = True, indent = 4)
+    with open(os.path.join(save_dir, "total_dataset_class_count.json"), "w") as fp:
+        json.dump(object_class_count_dict, fp, sort_keys = True, indent = 4)
+
+    frame_object_count_plot.get_figure().savefig(os.path.join(save_dir, "total_frame_object_count.png"))
+    class_count_plot.get_figure().savefig(os.path.join(save_dir, "total_dataset_class_count.png"))
+
     # Display the plots
     plt.show()
