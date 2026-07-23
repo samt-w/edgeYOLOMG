5766f272f51526ebab3439205db8330035d06c1b
Sam Taylor-Wilmshurst <samtaylorwilmshurst at gmail>
2026-07-23 14:01:27 +0100

    amending validation confidence threshold

--- train_cloud.py
+++ train_cloud.py
@@ -454,8 +454,8 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
                                             save_dir=save_dir,
                                             plots=False,
                                             callbacks=callbacks,
-                                            compute_loss=compute_loss)
-
+                                            compute_loss=compute_loss,
+                                            conf_thres=0.0001)
 
                 # Update best mAP
                 fi = fitness(np.array(results).reshape(1, -1))  # weighted combination of [P, R, mAP@.5, mAP@.5-.95]
