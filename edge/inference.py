e979690c0f6780dbf13e4de618ba1416a8283975
Sam Taylor-Wilmshurst <samtaylorwilmshurst at gmail>
2026-08-01 18:07:09 +0100

    initial definition of required functions

---
+++ edge/inference.py
@@ -0,0 +1,18 @@
+"""
+this script takes trained YOLOMG .pt weights and uses them to 
+detect drones in video input. It measures the performance of the detection
+using mAP and FPS.
+"""
+
+# read video
+# allow buffer to be initialised (to warm up GPU and for motion masks)
+# begin timer
+# extract frame
+# compute motion mask for each frame
+# make prediction
+# end timer
+# report accuracy and frame time
+# convert accuracy and frame time to mAP and FPS
+
+if __name__ == "__main__":
+    pass
